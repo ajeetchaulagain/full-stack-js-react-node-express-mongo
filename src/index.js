9b8f35869b90e2c53f731fdbcb72781b129a08d3
ajeetchaulagain <chaulagainajeet@gmail.com>
2019-12-04 00:00:04 +1100

    Just a commit to save"
    :wq

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 import App from './components/App';
+import data from './testData';
+console.log(data);
 
 // Add prop validation and default values
 ReactDOM.render(
